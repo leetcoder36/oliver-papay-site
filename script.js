be672bd45c5d23ccfec1a0384b99a9c3300491c9
opapay <oliver-papay@uiowa.edu>
2024-12-03 01:08:57 -0600

    basic site structure

---
+++ script.js
@@ -0,0 +1,11 @@
+const moveButton = document.getElementById("move-button")
+const bio = document.getElementById("bio")
+
+function move_bio() {
+    
+}
+
+moveButton.addEventListener('click', () => {
+    console.log("Button pressed")
+})
+
